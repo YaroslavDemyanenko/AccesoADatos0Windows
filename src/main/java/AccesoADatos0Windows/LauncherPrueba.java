3513bca2af56244fe80f38bf1f5173e9de4614ca
IN1DM3B_02 <IN1DM3B_02@H015112.NET>
2019-09-17 09:42:44 +0200

    Implementando Lector TXT (Windows)

---
+++ src/main/java/AccesoADatos0Windows/LauncherPrueba.java
@@ -0,0 +1,12 @@
+package AccesoADatos0Windows;
+
+import java.io.IOException;
+
+public class LauncherPrueba {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		LectorTxt ltxt = new LectorTxt(); 
+		ltxt.Comprobacion();	
+	}
+}
