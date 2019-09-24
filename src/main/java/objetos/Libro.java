8ee72b2da7f611202d57cbb5d9ee3faf486fcc6c
IN1DM3B_03 <IN1DM3B_03@H015112.NET>
2019-09-24 09:04:00 +0200

    Clase libro creada

--- src/main/java/objetos/Libro.java
+++ src/main/java/objetos/Libro.java
@@ -1,5 +1,17 @@
 package objetos;
 
 public class Libro {
-
+	int id;
+	String titulo,autor,genero,descripcion;
+	
+	
+	public Libro(int id, String titulo, String autor, String genero, String descripcion) {
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.genero = genero;
+		this.descripcion = descripcion;
+	}
+	
+	
 }
